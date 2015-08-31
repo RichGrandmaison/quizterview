4afb992eb20f4321d9c210c19f88a976f30810f1
rich <rich.grandmaison@gmail.com>
2015-08-31 13:30:37 -0400

    initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quizterviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quizterviewApp
+ */
+angular.module('quizterviewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
